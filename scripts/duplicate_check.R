d15c7dbac11a15d2c767250e61f78596de525335
Shayna Sura <sura@nceas.ucsb.edu>
2024-02-26 14:21:19 -0800

    setting up R project and R script to detect duplicates.

---
+++ scripts/duplicate_check.R
@@ -0,0 +1,15 @@
+## Gulf Ecosystem Initiative (GEI) - Offshore Wind Working Group
+## Ghoti Article
+
+## Checking for duplicates in the article lists from Web of Science
+
+### Ray did the original literature search in Web of Science on 
+### This first search returned 877 hits.
+
+### Shayna did an updated literature search in Web of Sciece on Monday, February 26, 2024.
+### This second search returned 925 hits.
+
+
+library(synthesisr)
+
+
